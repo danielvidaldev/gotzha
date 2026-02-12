d0945053733faff5bb63338b1200fdf04b8927b0
daniel <dvidalriezu@gmail.com>
2026-02-12 23:01:35 +0100

    Remove Ziggy dependency for Vercel compatibility

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,7 +4,6 @@ import '../css/app.css';
 import { createApp, h, DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { createPinia } from 'pinia';
 
 const appName = import.meta.env.VITE_APP_NAME || 'PrivateByRight VPN';
@@ -21,7 +20,6 @@ createInertiaApp({
 
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
             .use(pinia)
             .mount(el);
     },
